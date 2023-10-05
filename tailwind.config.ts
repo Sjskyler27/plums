d44492ebabad54833c3f8bf315b96301b9f897b7
Skyler Simpson <sjskyler27@gmail.com>
2023-10-05 17:05:14 -0600

    added colors and button component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -14,7 +14,13 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      byzantium: '#820263',
+      palePurple: '#F8DEFF',
+      skyMagenta: '#bd70b1',
+      plum: '#a0398a',
+    },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
