d637a1efe51f4d303e02819396e5ae644eef9bbe
Skyler Simpson <84102384+Sjskyler27@users.noreply.github.com>
2023-10-14 19:41:05 -0600

    topics are sowing up

--- src/app/examples/page.tsx
+++ src/app/examples/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import CustomButton from '@/components/UI/CustomButton';
 import Spinner from '@/components/SmallSpinner';
 import Card from '@/components/UI/BaseCard';
@@ -5,8 +6,15 @@ import BackgroundSpinner from '@/components/BackgroundSpinner';
 import Popup from '@/components/UI/CustomPopup';
 import Header from '@/components/layouts/Header';
 import Footer from '@/components/layouts/Footer';
+import TopicItem from '@/components/TopicItem';
 
 export default function Examples() {
+  const mathLinks = [
+    { text: 'MATH FOR THE REAL WORLD', url: '#path_to_pdf' },
+    { text: 'FIRST JUMP INTO CALCULUS', url: '#path_to_txt' },
+    { text: 'DERIVATIVES FOR NEWBIES', url: '#path_to_video' },
+    { text: 'TRIGONOMETRY EQUATIONS', url: '#path_to_page' },
+  ];
   return (
     <>
       <Header />
@@ -27,6 +35,10 @@ export default function Examples() {
         </Card>
         <div className="hidden">
           <BackgroundSpinner></BackgroundSpinner>
+        </div>
+        <div className="p-10">
+          <TopicItem title="MATHEMATICS" links={mathLinks} />
+          <TopicItem title="RELIGION" links={[]} />
         </div>
       </main>
       <Footer />
--- src/components/TopicItem.tsx
+++ src/components/TopicItem.tsx
@@ -1,5 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export const TopicItem = () => {
-  return <div>AccordionItem</div>;
+interface Link {
+  text: string;
+  url: string;
+}
+interface Props {
+  title: string;
+  links: Link[];
+}
+
+const TopicItem: React.FC<Props> = ({ title, links }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="w-full mb-4">
+      <div
+        className="bg-byzantium text-white text-lg font-bold p-4 cursor-pointer hover:byzantium"
+        onClick={toggleOpen}
+      >
+        {title}
+        <span className="float-right">
+          {isOpen ? <span>&#9650;</span> : <span>&#9660;</span>}
+        </span>
+      </div>
+      {isOpen && (
+        <div className="bg-white border-l-4 border-purple-500 pl-4 mt-2">
+          {links.map((link, index) => (
+            <a
+              key={index}
+              href={link.url}
+              className="block p-2 hover:bg-purple-100 transition-colors duration-300"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {link.text}
+            </a>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
+
+export default TopicItem;
