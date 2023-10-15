56568031f5ac8eca1156045959f32727de3d8785
Skyler Simpson <84102384+Sjskyler27@users.noreply.github.com>
2023-10-14 22:02:01 -0600

    added comments and change export

--- src/components/TopicItem.tsx
+++ src/components/TopicItem.tsx
@@ -12,7 +12,8 @@ interface Props {
   links: Link[];
 }
 
-const TopicItem: React.FC<Props> = ({ title, links }) => {
+export default function TopicItem({ title, links }: Props) {
+  // keep in mind use state requires use client on the page or component that uses it!
   const [isOpen, setIsOpen] = useState(false);
   const [contentHeight, setContentHeight] = useState<number | null>(null);
 
@@ -23,7 +24,7 @@ const TopicItem: React.FC<Props> = ({ title, links }) => {
     if (isOpen && cardRef.current) {
       const contentWrapper = cardRef.current.querySelector('#contentWrapper');
       if (contentWrapper) {
-        setContentHeight(contentWrapper.scrollHeight);
+        setContentHeight(contentWrapper.scrollHeight); // scroll height gives height even if content is hidden
       }
     } else {
       // Reset the content height when the card is closed
@@ -35,10 +36,11 @@ const TopicItem: React.FC<Props> = ({ title, links }) => {
     setIsOpen(!isOpen);
   };
 
+  // custom css style for the height of a card (little impossible to do with tailwind)
   const contentStyle = {
     maxHeight: contentHeight !== null ? `${contentHeight}px` : '0', // Use calculated height or 0 if closed
     overflow: 'hidden',
-    transition: 'max-height 0.4s ease', // Adjust the duration and easing as needed
+    transition: 'max-height 0.4s ease', // change value to affect the content drop speed
   };
 
   return (
@@ -46,11 +48,15 @@ const TopicItem: React.FC<Props> = ({ title, links }) => {
       <div
         id="header"
         className={`bg-byzantium text-white text-lg font-bold p-4 cursor-pointer hover:byzantium rounded-t-lg transition-all 
-        ${!isOpen ? 'rounded-b-lg transition-rounded duration-1000' : ''}`} // remove border slowly when closed
+        ${
+          !isOpen
+            ? 'rounded-b-lg transition-rounded duration-300 delay-300' // change value to affect radius delay and speed
+            : ''
+        }`} // remove border slowly when closed
         onClick={toggleOpen}
       >
         {title}
-        <span className="float-right">
+        <span id="arrowIcon" className="float-right">
           {isOpen ? <span>&#9650;</span> : <span>&#9660;</span>}
         </span>
       </div>
@@ -58,31 +64,34 @@ const TopicItem: React.FC<Props> = ({ title, links }) => {
         id="contentWrapper"
         className={`bg-palePurple border-l-4 border-r-4 border-skyMagenta pl-4 rounded-b-lg 
         ${
-          isOpen ? 'border-b-4 transition-border duration-300 delay-1000' : ''
+          isOpen ? 'border-b-4 transition-border duration-300 delay-1000' : '' // currently does not seem to be working trying to slowly remove the bottom border
         }`}
         style={contentStyle}
       >
-        {links.map((link, index) => (
-          <div id="linksIcoWrapper" key={index}>
-            <a
-              className={`flex items-center p-2 text-blue hover:text-darkBlue transition-colors duration-300`}
-              href={link.url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                src={`/icons/${link.type}.png`}
-                alt={`${link.type}`}
-                width={24}
-                height={24}
-              />
-              {link.text}
-            </a>
-          </div>
-        ))}
+        {links.map(
+          (
+            link,
+            index // dynamically add link text, url and image
+          ) => (
+            <div id="linksIcoWrapper" key={index}>
+              <a
+                className={`flex items-center p-2 text-blue hover:text-darkBlue transition-colors duration-300`}
+                href={link.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image
+                  src={`/icons/${link.type}.png`}
+                  alt={`${link.type}`}
+                  width={24}
+                  height={24}
+                />
+                {link.text}
+              </a>
+            </div>
+          )
+        )}
       </div>
     </div>
   );
-};
-
-export default TopicItem;
+}
