221eab7f1fe63d3a86ddc3e622d9270a747de42d
Skyler Simpson <84102384+Sjskyler27@users.noreply.github.com>
2023-10-19 17:44:37 -0600

    added pinstripe

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
   };
 
   return (
-    <header className="bg-white fixed top-0 w-full">
+    <header className="bg-white fixed top-0 w-full border-b-byzantium border-b-2">
       <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
