4a175d70dcd72c42e24a4d81a14c086550926f98
Skyler Simpson <84102384+Sjskyler27@users.noreply.github.com>
2023-11-03 12:13:49 -0600

    centererd spinner

--- src/components/SmallSpinner.tsx
+++ src/components/SmallSpinner.tsx
@@ -1,5 +1,7 @@
 export default function Spinner() {
   return (
-    <div className="w-6 h-6 border-t-2 border-plum rounded-full animate-spin"></div>
+    <div className="flex justify-center items-center h-full">
+      <div className="w-6 h-6 border-t-2 border-plum rounded-full animate-spin"></div>
+    </div>
   );
 }
