0751c3d6e7f57ceeac38b488f2607e128a470e2b
Skyler Simpson <84102384+Sjskyler27@users.noreply.github.com>
2023-11-28 10:30:19 -0700

    constants

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,2 +1,2 @@
-// export const apiBaseUrl = "https://plms.netlify.app/api";
-export const apiBaseUrl = 'http://localhost:3000/api';
+export const apiBaseUrl = 'https://plms.netlify.app/api';
+// export const apiBaseUrl = 'http://localhost:3000/api';
