1fe96611d6632862b43b5ce36cd44724f9cc4402
Skyler Simpson <84102384+Sjskyler27@users.noreply.github.com>
2023-12-05 11:21:43 -0700

    topic data comment

--- src/data/topicData.tsx
+++ src/data/topicData.tsx
@@ -1,101 +1,101 @@
-import LinkModel from './LinkModel';
+// import LinkModel from './LinkModel';
 
-export const mathLinks: LinkModel[] = [
-  {
-    text: 'MATH FOR THE REAL WORLD',
-    type: 'link', // Use a colon (:) for assignment, not an equal sign (=)
-    url: 'https://content.byui.edu/file/fb4c2d20-04c3-463d-93f6-725064a8b15d/18/fdmat108syllabus.html',
-  },
-  {
-    text: 'FIRST JUMP INTO CALCULUS',
-    type: 'youtube', // Use a colon (:) for assignment, not an equal sign (=)
-    url: 'https://www.youtube.com/watch?v=5yfh5cf4-0w',
-  },
-  {
-    text: 'DERIVATIVES FOR NEWBIES',
-    type: 'pdf', // Use a colon (:) for assignment, not an equal sign (=)
-    url: '#path_to_video',
-  },
-  {
-    text: 'TRIGONOMETRY EQUATIONS',
-    type: 'txt', // Use a colon (:) for assignment, not an equal sign (=)
-    url: '#path_to_page',
-  },
-];
-export const religionLinks = [
-  {
-    text: 'The Book of Mormon',
-    type: 'link',
-    url: 'https://www.lds.org/scriptures/bofm?lang=eng',
-  },
-  {
-    text: 'Understanding the Bible',
-    type: 'link',
-    url: 'https://www.bible.com/',
-  },
-  {
-    text: 'World Religions Overview',
-    type: 'link',
-    url: 'https://www.worldreligions.psu.edu/',
-  },
-  {
-    text: 'Religion and Philosophy Courses',
-    type: 'link',
-    url: 'https://www.coursera.org/specializations/religion-philosophy',
-  },
-  {
-    text: 'The Bhagavad Gita',
-    type: 'link',
-    url: 'https://www.bhagavad-gita.org/',
-  },
-  {
-    text: 'Introduction to Buddhism',
-    type: 'link',
-    url: 'https://www.learnreligions.com/an-introduction-to-buddhism-449635',
-  },
-  {
-    text: 'Islam: A Brief Introduction',
-    type: 'link',
-    url: 'https://www.islamreligion.com/',
-  },
-  {
-    text: 'Judaism 101',
-    type: 'link',
-    url: 'https://www.jewfaq.org/',
-  },
-  {
-    text: 'Hinduism Basics',
-    type: 'link',
-    url: 'https://www.hinduwebsite.com/',
-  },
-  {
-    text: 'Comparative Religion Studies',
-    type: 'link',
-    url: 'https://www.harvard.edu/academics/graduate-degrees/hds/comparative-religion',
-  },
-  {
-    text: 'The Vatican Official Website',
-    type: 'link',
-    url: 'https://www.vatican.va/content/vatican/en.html',
-  },
-  {
-    text: 'Religion News Service',
-    type: 'link',
-    url: 'https://religionnews.com/',
-  },
-  {
-    text: 'Religion and Ethics Newsweekly',
-    type: 'link',
-    url: 'https://www.pbs.org/wnet/religionandethics/',
-  },
-  {
-    text: 'Religious Studies Journals',
-    type: 'link',
-    url: 'https://www.jstor.org/journal/jrelstud',
-  },
-  {
-    text: 'Sacred Texts Archive',
-    type: 'link',
-    url: 'http://www.sacred-texts.com/',
-  },
-];
+// export const mathLinks: LinkModel[] = [
+//   {
+//     text: 'MATH FOR THE REAL WORLD',
+//     type: 'link', // Use a colon (:) for assignment, not an equal sign (=)
+//     url: 'https://content.byui.edu/file/fb4c2d20-04c3-463d-93f6-725064a8b15d/18/fdmat108syllabus.html',
+//   },
+//   {
+//     text: 'FIRST JUMP INTO CALCULUS',
+//     type: 'youtube', // Use a colon (:) for assignment, not an equal sign (=)
+//     url: 'https://www.youtube.com/watch?v=5yfh5cf4-0w',
+//   },
+//   {
+//     text: 'DERIVATIVES FOR NEWBIES',
+//     type: 'pdf', // Use a colon (:) for assignment, not an equal sign (=)
+//     url: '#path_to_video',
+//   },
+//   {
+//     text: 'TRIGONOMETRY EQUATIONS',
+//     type: 'txt', // Use a colon (:) for assignment, not an equal sign (=)
+//     url: '#path_to_page',
+//   },
+// ];
+// export const religionLinks = [
+//   {
+//     text: 'The Book of Mormon',
+//     type: 'link',
+//     url: 'https://www.lds.org/scriptures/bofm?lang=eng',
+//   },
+//   {
+//     text: 'Understanding the Bible',
+//     type: 'link',
+//     url: 'https://www.bible.com/',
+//   },
+//   {
+//     text: 'World Religions Overview',
+//     type: 'link',
+//     url: 'https://www.worldreligions.psu.edu/',
+//   },
+//   {
+//     text: 'Religion and Philosophy Courses',
+//     type: 'link',
+//     url: 'https://www.coursera.org/specializations/religion-philosophy',
+//   },
+//   {
+//     text: 'The Bhagavad Gita',
+//     type: 'link',
+//     url: 'https://www.bhagavad-gita.org/',
+//   },
+//   {
+//     text: 'Introduction to Buddhism',
+//     type: 'link',
+//     url: 'https://www.learnreligions.com/an-introduction-to-buddhism-449635',
+//   },
+//   {
+//     text: 'Islam: A Brief Introduction',
+//     type: 'link',
+//     url: 'https://www.islamreligion.com/',
+//   },
+//   {
+//     text: 'Judaism 101',
+//     type: 'link',
+//     url: 'https://www.jewfaq.org/',
+//   },
+//   {
+//     text: 'Hinduism Basics',
+//     type: 'link',
+//     url: 'https://www.hinduwebsite.com/',
+//   },
+//   {
+//     text: 'Comparative Religion Studies',
+//     type: 'link',
+//     url: 'https://www.harvard.edu/academics/graduate-degrees/hds/comparative-religion',
+//   },
+//   {
+//     text: 'The Vatican Official Website',
+//     type: 'link',
+//     url: 'https://www.vatican.va/content/vatican/en.html',
+//   },
+//   {
+//     text: 'Religion News Service',
+//     type: 'link',
+//     url: 'https://religionnews.com/',
+//   },
+//   {
+//     text: 'Religion and Ethics Newsweekly',
+//     type: 'link',
+//     url: 'https://www.pbs.org/wnet/religionandethics/',
+//   },
+//   {
+//     text: 'Religious Studies Journals',
+//     type: 'link',
+//     url: 'https://www.jstor.org/journal/jrelstud',
+//   },
+//   {
+//     text: 'Sacred Texts Archive',
+//     type: 'link',
+//     url: 'http://www.sacred-texts.com/',
+//   },
+// ];
